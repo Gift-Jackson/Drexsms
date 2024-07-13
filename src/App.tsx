f9a777e0781e19a4a9300abe25de457e97e72205
Gift Jacksun <giftjacksun@gmail.com>
2024-07-13 06:59:08 +0100

    Initial Commit: Setting Up Tailwind and Typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="h-screen grid place-items-center">
+        <h1 className="text-4xl font-bold underline text-indigo-500">Drexsms.com</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
